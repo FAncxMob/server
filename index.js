8b8b908614fb0b7916769d03e5a5a6762a3102fd
fan chongxin <fancx29@gmail.com>
2024-09-20 02:36:37 +0900

    cors

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const PORT = process.env.PORT || 5000;
 const PASSWORD = "fanchongxin";
 
 // 中间件
-app.use(
-  cors({
-    origin: "http://localhost:3000", // 前端地址
-    credentials: true, // 允许发送 cookies
-  })
-);
+app.use(cors());
 app.use(express.json()); // 解析 JSON 数据
 
 app.use(
