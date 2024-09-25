a38476c162dea30d669c308711bd7bed3ba221e4
fan chongxin <fancx29@gmail.com>
2024-09-26 00:05:42 +0900

    header

--- index.js
+++ index.js
@@ -282,10 +282,13 @@ app.get("/api/getNovelInfo", async (req, res) => {
     });
 
     const response = await axios.get(`https://ncode.syosetu.com/${ncode}`, {
-      // httpsAgent: proxyAgent, // 通过代理发送请求
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        Connection: "keep-alive",
       },
     });
     console.error("/api/getNovelInfo:response", response.data);
